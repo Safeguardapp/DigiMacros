cd7e2d23865224a5b24eccb4b7743eaaabfa0eaf
Gertjan Leemans <gertjan@digitalisma.com>
2015-02-18 10:27:12 +0100

    Added GLXCTMacros

---
+++ GLXCTMacros.h
@@ -0,0 +1,9 @@
+#define XCTAssertIsValidUUIDString(test) \
+    NSUUID *_u = ([test isKindOfClass:[NSString class]] ? [[NSUUID alloc] initWithUUIDString:(test)] : nil); \
+    if (_u == nil) { \
+        XCTFail(@"'%@' is not a valid UUID string", test); \
+    } 
+#define XTCAssertIsKindOfClass(test, expression) \
+    if([test isKindOfClass: expression]) { \
+        XCTFail(@"'%@' is not of class %@", test, expression); \
+    } 
